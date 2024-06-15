7188706ad2b8ac35258e3d69d7355da39de4cce0
Cameron Cochrane <cochranecameron1@gmail.com>
2024-06-15 14:12:10 +0100

    6

--- tcs_charts.py
+++ tcs_charts.py
@@ -2,12 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+from streamlit_option_menu import option_menu
 import TCS_pipeline as tcs
 
-usa_data = tcs.TCSpipelineCountry(country='USA')
-tcs.fill_m_q_p(usa_data)
 
-usa_chart = tcs.return_plot(data=usa_data,country_name='USA')
+def usa_page():
+    data = tcs.TCSpipelineCountry(country='USA')
+    tcs.fill_m_q_p(data)
 
-st.pyplot(fig=usa_chart,use_container_width=True)
+    usa_chart = tcs.return_plot(data=data,country_name='USA')
 
+    st.title("USA Sales Data")
+    st.pyplot(fig=usa_chart,use_container_width=True)
+
+
+with st.sidebar:
+    selected = option_menu(menu_title="Main Menu",
+        options=["USA", "UK", "Germany"],
+        icons=["house", "file-earmark", "file-earmark"],
+        menu_icon="cast",
+        default_index=0)
+
+if selected == "USA":
+    usa_page()
