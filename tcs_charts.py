358379b7e5fe68edc4b66f9f9a285e47f4cef6df
Cameron Cochrane <cochranecameron1@gmail.com>
2024-06-16 17:50:43 +0100

    9

--- tcs_charts.py
+++ tcs_charts.py
@@ -6,43 +6,26 @@ from streamlit_option_menu import option_menu
 import TCS_pipeline as tcs
 
 
+def show_country_page(country:str):
+    data = tcs.TCSpipelineCountry(country=country)
+    tcs.fill_m_q_p(data)
+
+    data_chart = tcs.return_plot(data=data,country_name=country)
+
+    st.title(f"{country} Sales Data")
+    st.pyplot(fig=data_chart,use_container_width=True)
+
+    tcs.name_columns_rows(data)
+    tcs.add_df_titles(df_list=data,country=country)
+
+    for i in range(0,len(data)):
+        st.dataframe(data[i],use_container_width=True)
+
 def home_page():
     st.title("Toy Car Sales Pipeline")
     st.write("This is a Streamlit dashboard which allows you to view the sales data of the Toy Car Sales dataset.")
     st.write("The dataset is a sample of sales data from a company selling toys. The data is formatted in a way that allows us to easily view the sales figures for each country.")
 
-def usa_page():
-    data = tcs.TCSpipelineCountry(country='USA')
-    tcs.fill_m_q_p(data)
-
-    usa_chart = tcs.return_plot(data=data,country_name='USA')
-
-    st.title("USA Sales Data")
-    st.pyplot(fig=usa_chart,use_container_width=True)
-    
-    tcs.name_columns_rows(data)
-    tcs.add_df_titles(df_list=data,country='USA')
-
-    for i in range(0,len(data)):
-        st.dataframe(data[i],use_container_width=True)
-
-def uk_page():
-    data = tcs.TCSpipelineCountry(country='UK')
-    tcs.fill_m_q_p(data)
-
-    uk_chart = tcs.return_plot(data=data,country_name='UK')
-
-    st.title("UK Sales Data")
-    st.pyplot(fig=uk_chart,use_container_width=True)
-
-def germany_page():
-    data = tcs.TCSpipelineCountry(country='Germany')
-    tcs.fill_m_q_p(data)
-
-    germany_chart = tcs.return_plot(data=data,country_name='Germany')
-
-    st.title("Germany Sales Data")
-    st.pyplot(fig=germany_chart,use_container_width=True)
 
 with st.sidebar:
     selected = option_menu(menu_title="Main Menu",
@@ -53,9 +36,6 @@ with st.sidebar:
 
 if selected == "Home":
     home_page()
+
 if selected == "USA":
-    usa_page()
-if selected == "UK":
-    uk_page()
-if selected == "Germany":
-    germany_page()
+    show_country_page('USA')
